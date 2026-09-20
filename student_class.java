92b8c486125e31bac313829931e43b0334cf8c8d
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-09-20 21:07:42 +0530

    Create student_class.java

---
+++ student_class.java
@@ -0,0 +1,12 @@
+static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name,int rank){
+            this.name=name;
+            this.rank=rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank-s2.rank;
+        }
+  }
