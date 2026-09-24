6fca337ba5bd39cfc297d1c9509f6690beefc8d0
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-09-24 21:34:25 +0530

    Create Point_class.java

---
+++ Point_class.java
@@ -0,0 +1,16 @@
+static class Point implements Comparable<Point>{
+        int x;
+        int y;
+        int distSq;
+        int idx;
+        public Point(int x,int y,int distSq,int idx){
+            this.x=x;
+            this.y=y;
+            this.distSq=distSq;
+            this.idx=idx;
+        }
+        @Override
+        public int compareTo(Point p2){
+            return this.distSq-p2.distSq;
+        }
+    }
